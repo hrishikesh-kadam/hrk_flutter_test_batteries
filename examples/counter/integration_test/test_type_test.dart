804a0a68336d353395bafaadbb1da979cd7d0134
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-07-05 01:34:39 +0530

    Add counter example for integration test

--- examples/counter/integration_test/test_type_test.dart
+++ examples/counter/integration_test/test_type_test.dart
@@ -3,16 +3,12 @@ import 'package:hrk_flutter_test_batteries/hrk_flutter_test_batteries.dart';
 
 void main() {
   group('getTestType()', () {
-    test('Widget in test()', () {
-      expect(getTestType(), 'Widget');
-    });
-
-    testWidgets('Widget in testWidget()', (tester) async {
-      expect(getTestType(), 'Widget');
+    test('Integration in test()', () {
+      expect(getTestType(), 'Integration');
     });
 
     testWidgets('Integration in testWidgets()', (tester) async {
       expect(getTestType(), 'Integration');
-    }, tags: ['get-test-type-integration', 'non-default']);
+    });
   });
 }
