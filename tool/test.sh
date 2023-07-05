379422e555add660c00bc33beea6ab7527e86aa7
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-07-05 18:10:56 +0530

    4th commit

--- tool/test.sh
+++ tool/test.sh
@@ -7,16 +7,18 @@ dart pub global activate coverage
 flutter test --coverage
 lcov --list coverage/lcov.info
 
-# https://github.com/flutter/flutter/issues/93567
 pushd examples/counter &> /dev/null
 if [[ $(uname -s) =~ ^"Linux" ]]; then
   OS_NAME="linux"
+  if [[ $GITHUB_ACTIONS ]]; then
+    # https://github.com/flutter/flutter/issues/93567
+    # https://stackoverflow.com/a/76620317/3302026
+    DISPLAY_SERVER="xvfb-run"
+  fi
 elif [[ $(uname -s) =~ ^"Darwin" ]]; then
   OS_NAME="macos"
 elif [[ $(uname -s) =~ ^"MINGW" ]]; then
   OS_NAME="windows"
 fi
-if [[ -n $OS_NAME ]]; then
-  xvfb-run flutter test -d "$OS_NAME" integration_test
-fi
+$DISPLAY_SERVER flutter test -d "$OS_NAME" integration_test
 popd &> /dev/null
