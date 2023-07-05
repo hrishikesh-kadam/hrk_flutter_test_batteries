fe041b205f5cc2de23a3ed8c8577f609c5dc20ac
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-07-05 16:49:22 +0530

    1st commit

--- tool/test.sh
+++ tool/test.sh
@@ -7,17 +7,15 @@ dart pub global activate coverage
 flutter test --coverage
 lcov --list coverage/lcov.info
 
-if [[ ! $GITHUB_ACTIONS ]]; then
-  pushd examples/counter &> /dev/null
-  if [[ $(uname -s) =~ ^"Linux" ]]; then
-    OS_NAME="linux"
-  elif [[ $(uname -s) =~ ^"Darwin" ]]; then
-    OS_NAME="macos"
-  elif [[ $(uname -s) =~ ^"MINGW" ]]; then
-    OS_NAME="windows"
-  fi
-  if [[ -n $OS_NAME ]]; then
-    flutter test -d "$OS_NAME" integration_test
-  fi
-  popd &> /dev/null
+pushd examples/counter &> /dev/null
+if [[ $(uname -s) =~ ^"Linux" ]]; then
+  OS_NAME="linux"
+elif [[ $(uname -s) =~ ^"Darwin" ]]; then
+  OS_NAME="macos"
+elif [[ $(uname -s) =~ ^"MINGW" ]]; then
+  OS_NAME="windows"
 fi
+if [[ -n $OS_NAME ]]; then
+  flutter test -d "$OS_NAME" integration_test
+fi
+popd &> /dev/null
