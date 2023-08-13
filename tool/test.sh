48fd450a9dee0c566aebdacced2c1c7683d582a7
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-08-13 23:35:31 +0530

    Enable integration_test on GitHub Actions
    This reverts commit 740ee081827c6f3b41ee22e138184f82ccddef00.

--- tool/test.sh
+++ tool/test.sh
@@ -7,22 +7,19 @@ dart pub global activate coverage
 flutter test --coverage
 lcov --list coverage/lcov.info
 
-# Disable temporarily till the apt dependency libgtk-3-dev is fixed
-if [[ ! $GITHUB_ACTIONS ]]; then
-  pushd examples/counter &> /dev/null
-  flutter pub get
-  if [[ $(uname -s) =~ ^"Linux" ]]; then
-    OS_NAME="linux"
-    if [[ $GITHUB_ACTIONS ]]; then
-      # https://github.com/flutter/flutter/issues/93567
-      # https://stackoverflow.com/a/76620317/3302026
-      DISPLAY_SERVER="xvfb-run"
-    fi
-  elif [[ $(uname -s) =~ ^"Darwin" ]]; then
-    OS_NAME="macos"
-  elif [[ $(uname -s) =~ ^"MINGW" ]]; then
-    OS_NAME="windows"
+pushd examples/counter &> /dev/null
+flutter pub get
+if [[ $(uname -s) =~ ^"Linux" ]]; then
+  OS_NAME="linux"
+  if [[ $GITHUB_ACTIONS ]]; then
+    # https://github.com/flutter/flutter/issues/93567
+    # https://stackoverflow.com/a/76620317/3302026
+    DISPLAY_SERVER="xvfb-run"
   fi
-  $DISPLAY_SERVER flutter test -d "$OS_NAME" integration_test
-  popd &> /dev/null
+elif [[ $(uname -s) =~ ^"Darwin" ]]; then
+  OS_NAME="macos"
+elif [[ $(uname -s) =~ ^"MINGW" ]]; then
+  OS_NAME="windows"
 fi
+$DISPLAY_SERVER flutter test -d "$OS_NAME" integration_test
+popd &> /dev/null
